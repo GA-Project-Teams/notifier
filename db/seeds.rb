389d3064a18c9d79720e48444309c1e74791f1b0
Michael Green <greenm2@gmail.com>
2012-08-08 20:48:10 -0400

    Fixed Login Bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: 'test@test.com', first_name: 'test', last_name: 'admin', password: 'test123', password_confirmation: 'test123')
+User.create(email: 't@test.com', first_name: 'test', last_name: 'admin', password: 'asd', password_confirmation: 'asd')
