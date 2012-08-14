e022576d6db9e53b5f32ff835b7f10cbaa08ee0c
Peter To <pt9386@gmail.com>
2012-08-14 01:26:14 -0400

    Refactors unit tests for message and user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,12 @@
 FactoryGirl.define do
   factory :user do
 
-    email Faker::Internet.email
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
-    company_name Faker::Company.name
-    phone_number Faker::PhoneNumber.phone_number
-    preference 1
+    email { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    company_name { Faker::Company.name }
+    phone_number { Faker::PhoneNumber.phone_number }
+    preference { rand(2) }
     password "password12345"
     password_confirmation "password12345"
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ Spork.prefork do
   require 'capybara/firebug'
   require 'database_cleaner'
   require 'email_spec'
+  require 'shoulda-matchers'
   
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
