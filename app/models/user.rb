72a64fe665d4058cf2c6bf3d4d03cf8d19cf6c8a
mhamrah <mhamrah@gmail.com>
2012-08-15 15:22:37 -0400

    running annotate on routes and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,3 +18,33 @@ class User < ActiveRecord::Base
   validates :password_confirmation, :presence => true, :if => :enable_strict_validation
 
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  first_name             :string(255)
+#  last_name              :string(255)
+#  company_name           :string(255)
+#  phone_number           :string(255)
+#  preference             :integer
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,20 @@ Notifier::Application.routes.draw do
   root :to => "home#index"
   resources :users
 end
+#== Route Map
+# Generated on 15 Aug 2012 15:21
+#
+#         user_session POST   /users/login(.:format)         devise/sessions#create
+# destroy_user_session DELETE /users/logout(.:format)        devise/sessions#destroy
+#        user_password POST   /users/password(.:format)      devise/passwords#create
+#    new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+#   edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+#                      PUT    /users/password(.:format)      devise/passwords#update
+#                 root        /                              home#index
+#                users GET    /users(.:format)               users#index
+#                      POST   /users(.:format)               users#create
+#             new_user GET    /users/new(.:format)           users#new
+#            edit_user GET    /users/:id/edit(.:format)      users#edit
+#                 user GET    /users/:id(.:format)           users#show
+#                      PUT    /users/:id(.:format)           users#update
+#                      DELETE /users/:id(.:format)           users#destroy
