c0a7540a5c9af2a90a2bb5bb8a31ec20b0155537
aas395 <aas395@new-host-2.home>
2012-08-15 19:22:35 -0400

    committing updates to typeahead script

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,42 +2,22 @@
 // All this logic will automatically be available in application.js.
 $(function() {
 
-	$('.typeahead').typeahead({
-	      source: function (typeahead, query) {
+	$('input.typeahead').typeahead({
+      	source: function (typeahead, query) {
         	return $.get('/users.json', { query: query }, function (data) {
         		var return_list = [];
-        		var i = 0;
 
-        		while(i < data.length) {
+        		for (var i = 0; i < data.length; i++) {
         			var user = data[i];
-        			return_list.push({value: user.first_name + " " + user.last_name, id: user.id});
-        			i++;
-							console.log(user.id);
-							$("#message_to_user_id").val(user.id);
-
+        			user.name = user.first_name + " " + user.last_name;
+        			return_list.push({value: user.name, id: user.id});
         		}
 
-						if (user.preference === 1) {
-								$("#message_message_type_id_1").prop("checked", true);
-								$("#message_message_type_id_2").prop("checked", false);
-								$("#message_message_type_id_3").prop("checked", false);
-								
-								console.log(user.preference);
-						} else if ( user.preference === 2 ) {
-							$("#message_message_type_id_1").prop("checked", false);
-							$("#message_message_type_id_2").prop("checked", true);
-							$("#message_message_type_id_3").prop("checked", false);
-								console.log(user.preference);
-						} else {
-							$("#message_message_type_id_1").prop("checked", false);
-							$("#message_message_type_id_2").prop("checked", false);
-							$("#message_message_type_id_3").prop("checked", true);
-								console.log(user.preference);
-						}
-
-        		return typeahead.process(return_list);
-						
+        		return typeahead.process(return_list);		
     		});
+        },
+        onselect: function(obj) {
+        	console.log('SDFSDFSDF');
         },
         property: 'value',
     	items: 5,
