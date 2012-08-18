18ee694837bafd69ccce5c84da2e78b4e6ad5776
Peter To <pt9386@gmail.com>
2012-08-17 21:26:02 -0400

    Removes broken javascript

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,27 +4,27 @@ $(function() {
 
 	
 	// $("td.links").hide();
-	$("tr").live("hover",
-	  function () {
-	    // $(this).("td.links").show();
-	    // $(this).children.show();
-	    // $(this).next.show();
-	    $(this).find("td.links").show();
-	    // console.log($(this).find("td.links"));
-	     // $(this).append($("<span> ***</span>"));
-	  }
-	  // function() {
-	  // 	// $(this).find("td.links").hide();
-	  // }
-	);
+	// $("tr").live("hover",
+	//   function () {
+	//     // $(this).("td.links").show();
+	//     // $(this).children.show();
+	//     // $(this).next.show();
+	//     $(this).find("td.links").show();
+	//     // console.log($(this).find("td.links"));
+	//      // $(this).append($("<span> ***</span>"));
+	//   }
+	//   // function() {
+	//   // 	// $(this).find("td.links").hide();
+	//   // }
+	// );
 
-	$("tr").live("hover",
-		function(){
-		// $(this).fadeOut(100);
-		$(this).find("td.links").fadeIn(500);
-		$(this).find("td.links").fadeOut(200);
+	// $("tr").live("hover",
+	// 	function(){
+	// 	// $(this).fadeOut(100);
+	// 	$(this).find("td.links").fadeIn(500);
+	// 	$(this).find("td.links").fadeOut(200);
 
-		$(this).fadeIn(150);
-		// $("td.links").fadeOut(100);
-	});
+	// 	$(this).fadeIn(150);
+	// 	// $("td.links").fadeOut(100);
+	// });
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,5 @@
 class MessagesController < ApplicationController
-  load_and_authorize_resource
   before_filter :authenticate_user!
-  
   def new
     @message = Message.new
   end
