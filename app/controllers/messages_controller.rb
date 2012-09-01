3627887b55877734fec0f44f854aec6b5b854bee
Peter To <pt9386@gmail.com>
2012-09-01 02:58:08 -0400

    Refactors messages resources to move message sending to an after_save callback instead of within messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,9 +9,7 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(params[:message])
-    @user = User.find(@message.user_id)
     if @message.save
-      @message.send_notification(@user, @message)
       redirect_to(new_message_path, :notice => 'Notification sent!')
     else
       render action: 'new'
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :user
+  after_save :send_notification
   # attr_accessible :content, :user_id, :from_user_id, :message_type_id
   attr_accessible :content, :user_id, :from_user_id, :message_type_id
   
@@ -7,19 +8,20 @@ class Message < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :from_user_id, :presence => true
   validates :message_type_id, :presence => true
-  
-  def send_notification(to_user, message)
-    if (message.message_type_id == 3)
-      send_email(to_user, message)
-      send_text_message(to_user, message)
-    elsif (message.message_type_id == 2)
-      send_email(to_user, message)
-    elsif (message.message_type_id == 1)
-      send_text_message(to_user, message)
+
+  def send_notification
+    if (self.message_type_id == 3)
+      send_email
+      send_text_message
+    elsif (self.message_type_id == 2)
+      send_email
+    elsif (self.message_type_id == 1)
+      send_text_message
     end
   end
-  
-  def send_text_message(to_user, message)
+
+  def send_text_message
+    user = User.find(self.user_id)
     twilio_sid = ENV['TWILIO_SID']
     twilio_token = ENV['TWILIO_TOKEN']
     twilio_phone_number = ENV['TWILIO_PHONE_NUMBER']
@@ -28,12 +30,14 @@ class Message < ActiveRecord::Base
     
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone_number}",
-      :to => to_user.phone_number,
-      :body => message.content
+      :to => user.phone_number,
+      :body => self.content
     )
   end
   
-  def send_email(to_user, message)
-    UserMailer.notify_user(to_user, message).deliver
+  def send_email
+    user = User.find(self.user_id) 
+    UserMailer.notify_user(user, self).deliver
   end
+
 end
